4c79eaca69d0d69f7d06eb13e04d409f2457257b
Prince Kelly <anyeprincekelly816@gmail.com>
2023-03-24 12:50:58 +0100

    new work

---
+++ Removing_names/nameDatabase/names.py
@@ -0,0 +1,5 @@
+from removing_names import *
+
+database =['kelly', 'junior', 'edrick', 'jb', 'clinton']
+name = input("Enter the name you want to remove form the database: ")
+remove_name(database, name.lower())
---
+++ Removing_names/removing_names.py
@@ -0,0 +1,9 @@
+
+def remove_name(list, name):
+        if name in list:
+            list.remove(name)
+            print('name removed')
+        else:
+            print("name not found in the list")
+
+
